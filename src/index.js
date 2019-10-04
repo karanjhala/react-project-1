610a90932ef453a448105f845abac4b6e794b71c
karanjhala <karanjhala@github.com>
2019-10-04 11:22:33 +0800

    Using JSX and creating elements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'Arial'
+}
+/* Without JSX
+    const title= React.createElement(
+//     'ul',
+//     {id: 'title', className: 'header', style:style},
+//     React.createElement('li',
+//     {},
+//     'item on our list'
+//     )
+    )
 
-const title= React.createElement(
-    'h1',
-    {id: 'title', className: 'header'},
-    'Hello World'
-)
 
 ReactDOM.render(
     title,
+    document.getElementById('root')
+)
+*/
+
+/* This is rendering with JSX */
+ReactDOM.render (
+    <div style={style}> 
+        <h1>Hello World!</h1>
+        <p> You Made it to using JSX!!</p>
+    </div>,
     document.getElementById('root')
 )
