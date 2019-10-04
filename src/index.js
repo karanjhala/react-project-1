2c03b6f76c289f255cc04ae303bb3a03d02a4b83
karanjhala <karanjhala@github.com>
2019-10-04 11:32:56 +0800

    Create First Component

--- src/index.js
+++ src/index.js
@@ -23,11 +23,35 @@ ReactDOM.render(
 )
 */
 
-/* This is rendering with JSX */
+/* This is rendering with JSX 
 ReactDOM.render (
     <div style={style}> 
         <h1>Hello World!</h1>
         <p> You Made it to using JSX!!</p>
     </div>,
+    document.getElementById('root')
+)
+*/
+/* Create a React Component */
+class Message extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Everyone, How's it going? I am a component</h1>
+            </div>
+        ) 
+    }
+}
+/*
+Component name is supposed to have First letter in capital,
+in order to differenciate it from other JSX elements.
+
+Make sure spacing is correct too
+
+
+*/
+
+ReactDOM.render( 
+    <Message />,
     document.getElementById('root')
 )
